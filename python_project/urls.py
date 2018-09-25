41c355630c004eb97dc02ee0859cbf1a5937aef9
Ahmedsebit <Ahmed.yusuf@andela.com>
2018-09-25 11:55:13 +0300

    [CHORE] -- Add first view

--- python_project/urls.py
+++ python_project/urls.py
@@ -16,6 +16,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.conf import settings
+from django.conf.urls import url
+from django.conf.urls.static import static
+
+
+from .views import home
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^$', home, name='home'),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += (static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
